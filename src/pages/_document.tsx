4a37120ece358adf4dd60a707772cc8d31ef2caf
공지운 <contact@bukgeuk.dev>
2022-02-26 22:26:36 +0900

    css optimization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default class document extends Document {
                     <link rel="icon" type="image/png" href="/image/icon/favicon-32.png" sizes="32x32"/>
                     <link rel="icon" type="image/png" href="/image/icon/favicon-152.png" sizes="152x152"/>
                     <link rel="icon" type="image/png" href="/image/icon/favicon-256.png" sizes="256x256"/>
+                    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"/>
                     <meta charSet="utf-8"></meta>
                     <meta name="theme-color" content="#6B1AE7"/>
                     <body>
